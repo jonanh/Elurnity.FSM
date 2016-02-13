01a7db24d65d85df5b1d613f97989677de420c69
Jon Ander Hernandez <jonan.h@gmail.com>
2016-02-13 12:04:51 +0200

    Add delegate state

--- State.cs
+++ State.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 using Events;
 using AssetBundles;
 
@@ -19,6 +20,37 @@ namespace FSM
 
         public virtual void exit()
         {
+        }
+    }
+    
+    public class DelegateState : State
+    {
+        public Action onEnter;
+        public Action onUpdate;
+        public Action onExit;
+        
+        public override void enter()
+        {
+            if (onEnter != null)
+            {
+                onEnter();
+            }
+        }
+
+        public override void update()
+        {
+            if (onUpdate != null)
+            {
+                onUpdate();
+            }
+        }
+
+        public override void exit()
+        {
+            if (onExit != null)
+            {
+                onExit();
+            }
         }
     }
     
