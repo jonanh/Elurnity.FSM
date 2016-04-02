99d49a6c314aa4322e5af9ada59f81409ab459a0
Jon Ander Hernandez <jonan.h@gmail.com>
2016-04-02 05:04:31 +0300

    Generalize TextAsset extension for reading and saving data as JSON

--- FSMExtensions.cs
+++ FSMExtensions.cs
@@ -13,22 +13,12 @@ namespace FSM
     {
         public static FSM fsm(this TextAsset textAsset)
         {
-            return JSON.Deserialize<FSM>(textAsset.text);
+            return textAsset.Deserialize<FSM>();
         }
 
         public static void fsm(this TextAsset textAsset, FSM fsm)
         {
-#if UNITY_EDITOR
-            var path = AssetDatabase.GetAssetPath(textAsset);
-            var dir = Application.dataPath.Replace("Assets", "");
-            path = Path.Combine(dir, path);
-
-            using (StreamWriter writer = new StreamWriter(path, false))
-            {
-                writer.Write(JSON.Serialize<FSM>(fsm, true));
-                writer.Close();
-            }
-#endif
+            textAsset.Serialize(fsm);
         }
     }
 }
